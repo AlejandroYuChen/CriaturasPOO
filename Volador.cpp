c5ebee4f64315673f4fd78ea29ee71c7a1fcb5ec
AlejandroYuChen <alejandroyu2@javerianacali.edu.co>
2025-05-07 16:54:20 -0500

    Delete Volador.cpp

--- Volador.cpp
+++
@@ -1,9 +0,0 @@
-#include "Volador.h"
-#include <iostream>
-
-Volador::Volador(const std::string& nombre, int salud, int fuerza)
-    : Criatura(nombre, salud, fuerza) {}
-
-void Volador::volar() {
-    std::cout << nombre << " vuela alto en el cielo.\n";
-}
