7005ecfee8b3f26d33934a07ab4c3eae0e760d84
AlejandroYuChen <alejandroyu2@javerianacali.edu.co>
2025-05-07 16:52:50 -0500

    Delete Acuatica.cpp

--- Acuatica.cpp
+++
@@ -1,9 +0,0 @@
-#include "Acuatica.h"
-#include <iostream>
-
-Acuatica::Acuatica(const std::string& nombre, int salud, int fuerza)
-    : Criatura(nombre, salud, fuerza) {}
-
-void Acuatica::nadar() {
-    std::cout << nombre << " nada velozmente en el agua.\n";
-}
